1081df88c793b104bc0e6d006d5559cba811e047
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2020-04-16 15:38:57 +0300

    debug

--- app/app.py
+++ app/app.py
@@ -93,4 +93,4 @@ def write_db(dict):
     return result.inserted_id
 
 if __name__=='__main__':
-    app.run(debug=True, host='127.0.0.1', port = 5000)
+    app.run(debug=True, host='0.0.0.0', port = 5000)
